81d629145985d3fcfbf6960970579d7ce4f6b248
KrzysztofP90 <krzysztof1.przybylowicz@gmail.com>
2019-07-03 11:30:52 +0200

    Implement first version of logic.

--- src/main/java/com/Krzysztof/App.java
+++ src/main/java/com/Krzysztof/App.java
@@ -6,5 +6,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello HSBC!" );
+        BracketChecker checker = new BracketChecker();
+
+        System.out.println(checker.areBracketsMatchedAndNestedCorrectly("my tes(st ( )( ))"));
     }
 }
--- src/main/java/com/Krzysztof/BracketChecker.java
+++ src/main/java/com/Krzysztof/BracketChecker.java
@@ -2,8 +2,38 @@ package com.Krzysztof;
 
 public class BracketChecker {
 
+    private final char openBracket = '[';
+    private final char closeBracket = ']';
+    private final char openBrace = '{';
+    private final char closeBrace = '}';
+    private final char openParenthes = '(';
+    private final char closeParenthes = ')';
+
     public boolean areBracketsMatchedAndNestedCorrectly(String textToCheck) {
-        
-        return true;
+
+        int countOfBrackets = 0;
+        int countOfBraces = 0;
+        int countOfParenthes = 0;
+
+        char[] textArray = textToCheck.toCharArray();
+
+        for (char ch : textArray) {
+            switch (ch) {
+                case openBracket: countOfBrackets ++;
+                break;
+                case closeBracket: countOfBrackets --;
+                break;
+                case openBrace: countOfBraces ++;
+                break;
+                case closeBrace: countOfBraces --;
+                break;
+                case openParenthes: countOfParenthes ++;
+                break;
+                case closeParenthes: countOfParenthes --;
+            }
+        }
+        if (countOfBrackets == 0 && countOfBraces == 0 && countOfParenthes == 0) {
+            return true;
+        } else return false;
     }
 }
